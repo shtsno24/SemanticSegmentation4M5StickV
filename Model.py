047af350d73daac667822cf49660878941302d4c
shtsno24 <shtsno.24@gmail.com>
2020-02-21 00:16:18 +0900

    add new model

--- Model.py
+++ Model.py
@@ -134,3 +134,93 @@ def TestNet(input_shape= (112, 112, 3), classes=151):
 
     model = Model(inputs, outputs)
     return model
+
+
+def TestNet2(input_shape=(112, 112, 3), classes=151):
+    inputs = Input(shape=input_shape)
+
+    x = MaxPooling2D(pool_size=(2, 2))(inputs)
+    x_3 = DepthwiseConv2D((3, 3), padding="same")(x)
+    x_5 = DepthwiseConv2D((5, 5), padding="same")(x)
+    x_7 = DepthwiseConv2D((7, 7), padding="same")(x)
+    x_9 = DepthwiseConv2D((9, 9), padding="same")(x)
+    x = Concatenate()([x, x_3, x_5, x_7, x_9])
+    x = Conv2D(64, (1, 1), activation="relu")(x)
+
+    # 56 x 56 x 64
+
+    x = MaxPooling2D(pool_size=(2, 2))(x)
+    x_3 = DepthwiseConv2D((3, 1), padding="same", activation="relu")(x)
+    x_3 = DepthwiseConv2D((1, 3), padding="same", activation="relu")(x_3)
+    x_3 = Conv2D(64, (1, 1), activation="relu")(x_3)
+    x_7 = DepthwiseConv2D((7, 1), padding="same", activation="relu")(x)
+    x_7 = DepthwiseConv2D((1, 7), padding="same", activation="relu")(x_7)
+    x_7 = Conv2D(64, (1, 1), activation="relu")(x_7)
+    x = Concatenate()([x, x_3, x_7])
+    x = Conv2D(128, (1, 1), activation="relu")(x)
+
+    # 28 x 28 x 128
+
+    x = MaxPooling2D(pool_size=(2, 2))(x)
+    x_3 = DepthwiseConv2D((3, 1), padding="same", activation="relu")(x)
+    x_3 = DepthwiseConv2D((1, 3), padding="same", activation="relu")(x_3)
+    x_3 = Conv2D(128, (1, 1), activation="relu")(x_3)
+    x_7 = DepthwiseConv2D((7, 1), padding="same", activation="relu")(x)
+    x_7 = DepthwiseConv2D((1, 7), padding="same", activation="relu")(x_7)
+    x_7 = Conv2D(128, (1, 1), activation="relu")(x_3)
+    x = Concatenate()([x, x_3, x_7])
+    x = Conv2D(256, (1, 1), activation="relu")(x)
+
+    # 14 x 14 x 256
+
+    x_3 = DepthwiseConv2D((3, 1), padding="same", activation="relu")(x)
+    x_3 = DepthwiseConv2D((1, 3), padding="same", activation="relu")(x_3)
+    x_3 = Conv2D(256, (1, 1), activation="relu")(x_3)
+    x_7 = DepthwiseConv2D((7, 1), padding="same", activation="relu")(x)
+    x_7 = DepthwiseConv2D((1, 7), padding="same", activation="relu")(x_7)
+    x_7 = Conv2D(256, (1, 1), activation="relu")(x_3)
+    x = Concatenate()([x, x_3, x_7])
+    x = Conv2D(256, (1, 1), activation="relu")(x)
+
+    # 14 x 14 x 256
+
+    x = UpSampling2D(size=(2, 2))(x)
+    x_3 = DepthwiseConv2D((3, 1), padding="same", activation="relu")(x)
+    x_3 = DepthwiseConv2D((1, 3), padding="same", activation="relu")(x_3)
+    x_3 = Conv2D(256, (1, 1), activation="relu")(x_3)
+    x_7 = DepthwiseConv2D((7, 1), padding="same", activation="relu")(x)
+    x_7 = DepthwiseConv2D((1, 7), padding="same", activation="relu")(x_7)
+    x_7 = Conv2D(256, (1, 1), activation="relu")(x_3)
+    x = Concatenate()([x, x_3, x_7])
+    x = Conv2D(128, (1, 1), activation="relu")(x)
+
+    # 28 x 28 x 128
+
+    x = UpSampling2D(size=(2, 2))(x)
+    x_3 = DepthwiseConv2D((3, 1), padding="same", activation="relu")(x)
+    x_3 = DepthwiseConv2D((1, 3), padding="same", activation="relu")(x_3)
+    x_3 = Conv2D(128, (1, 1), activation="relu")(x_3)
+    x_7 = DepthwiseConv2D((7, 1), padding="same", activation="relu")(x)
+    x_7 = DepthwiseConv2D((1, 7), padding="same", activation="relu")(x_7)
+    x_7 = Conv2D(128, (1, 1), activation="relu")(x_3)
+    x = Concatenate()([x, x_3, x_7])
+    x = Conv2D(64, (1, 1), activation="relu")(x)
+
+    # 56 x 56 x 64
+
+    x = UpSampling2D(size=(2, 2))(x)
+    x_3 = DepthwiseConv2D((3, 1), padding="same", activation="relu")(x)
+    x_3 = DepthwiseConv2D((1, 3), padding="same", activation="relu")(x_3)
+    x_3 = Conv2D(64, (1, 1), activation="relu")(x_3)
+    x_7 = DepthwiseConv2D((7, 1), padding="same", activation="relu")(x)
+    x_7 = DepthwiseConv2D((1, 7), padding="same", activation="relu")(x_7)
+    x_7 = Conv2D(64, (1, 1), activation="relu")(x_3)
+    x = Concatenate()([x, x_3, x_7])
+    x = Conv2D(classes, (1, 1), activation="relu")(x)
+
+    # 112 x 112 x classes
+
+    outputs = Activation("softmax")(x)
+
+    model = Model(inputs, outputs)
+    return model
