c3e68be5203d0620ea62d5ac3ece9b26348eb66f
shtsno24 <shtsno.24@gmail.com>
2020-02-23 00:47:44 +0900

    Add Resizer

--- VOC2012/data/Resize_VOC2012_npz.py
+++ VOC2012/data/Resize_VOC2012_npz.py
@@ -11,7 +11,7 @@ try:
     tf.add(1, 1) # To show INFO
     CROP_HEIGHT = 120
     CROP_WIDTH = 160
-    LABELS = 20
+    LABELS = 21
     COLOR_DEPTH = 3
     SAMPLE = np.random.randint(30)
 
@@ -26,6 +26,8 @@ try:
     image_file_directories = {"image": "./JPEGImages/", "annotation": "./SegmentationClass/"}
     image_array_list = []
     annotation_array_list = []
+    label_balance_array = np.zeros(LABELS)
+    label_balance_array_resize = np.zeros(LABELS)
 
     # Generate Train Data Records
     try:
@@ -44,6 +46,8 @@ try:
                     annotation_data = np.array(annotation_object, dtype=np.uint8)
                     annotation_data = annotation_data.reshape(annotation_data.shape + (1,))
                     annotation_data[annotation_data == 255] = 0
+                    hist, bin = np.histogram(annotation_data, bins=np.arange(LABELS + 1))
+                    label_balance_array += hist
                     if i == 0:
                         palette = np.array(annotation_object.getpalette(), dtype=np.uint8).reshape(-1, 3)
 
@@ -62,6 +66,8 @@ try:
                 annotation_data = tf.reshape(annotation_data, annotation_data.shape[:2])
                 annotation_array = annotation_data.numpy()
                 image_array = image_data.numpy()
+                hist, bin = np.histogram(annotation_array, bins=np.arange(LABELS + 1))
+                label_balance_array_resize += hist
 
                 image_array_list.append(image_array)
                 annotation_array_list.append(annotation_array)
@@ -74,138 +80,170 @@ try:
                 else:
                     show_flag = 0
 
-            annotation_data = annotation_array
-            annotation_image = Image.fromarray(annotation_data)
-            annotation_image.putpalette(palette)
-            annotation_image.show()
+        annotation_data = annotation_array
+        annotation_image = Image.fromarray(annotation_data)
+        annotation_image.putpalette(palette)
+        annotation_image.show()
 
+        annotation_data = np.array(annotation_array_list, dtype=np.uint8)
+        image_data = np.array(image_array_list, dtype=np.uint8)
+        np.savez(TRAIN_RECORDS,
+                 image=image_data,
+                 annotation=annotation_data,
+                 label_pix_resize=label_balance_array_resize,
+                 label_pix=label_balance_array)
+        print("Labels               :", bin)
+        print("Pixels in each label :", label_balance_array)
+        print("Pixels in each label(with resized images) :", label_balance_array)
         print("\n\nDone")
+
     except:
         traceback.print_exc()
         sys.exit(1)
 
-
     # Generate Train Validate Data Records
     try:
         print("\n\n\nGenerate Train Validate Data Records...\n")
-        with tf.io.TFRecordWriter(TRAINVAL_RECORDS) as writer:
-            with open(TRAINVAL_DATA_NAME) as f:
-                reader = csv.reader(f)
-                show_flag = 0
-                data_num = 2912
-                for i, name in enumerate(reader):
-                    # Loading image-like data
-                    image_file_name = image_file_directories["image"] + name[0] + ".jpg"
-                    annotation_file_name = image_file_directories["annotation"] + name[0] + ".png"
-                    with Image.open(image_file_name) as image_object:
-                        image_data = np.array(image_object, dtype=np.uint8)
-                    with Image.open(annotation_file_name) as annotation_object:
-                        annotation_data = np.array(annotation_object, dtype=np.uint8)
-                        annotation_data = annotation_data.reshape(annotation_data.shape + (1,))
-                        annotation_data[annotation_data == 255] = 0
-                        if i == 0:
-                            palette = np.array(annotation_object.getpalette(), dtype=np.uint8).reshape(-1, 3)
+        with open(TRAINVAL_DATA_NAME) as f:
+            reader = csv.reader(f)
+            show_flag = 0
+            data_num = 2912
+            for i, name in enumerate(reader):
+                # Loading image-like data
+                image_file_name = image_file_directories["image"] + name[0] + ".jpg"
+                annotation_file_name = image_file_directories["annotation"] + name[0] + ".png"
+                with Image.open(image_file_name) as image_object:
+                    image_data = np.array(image_object, dtype=np.uint8)
+                with Image.open(annotation_file_name) as annotation_object:
+                    annotation_data = np.array(annotation_object, dtype=np.uint8)
+                    annotation_data = annotation_data.reshape(annotation_data.shape + (1,))
+                    annotation_data[annotation_data == 255] = 0
+                    hist, bin = np.histogram(annotation_data, bins=np.arange(LABELS + 1))
+                    label_balance_array += hist
+                    if i == 0:
+                        palette = np.array(annotation_object.getpalette(), dtype=np.uint8).reshape(-1, 3)
 
-                    # Resizeing data
-                    X, Y = image_data.shape[0], image_data.shape[1]
-                    if X < Y:
-                        long_side = Y
-                    else:
-                        long_side = X
-                    annotation_data = tf.image.resize_with_crop_or_pad(annotation_data, long_side, long_side)
-                    annotation_data = tf.image.resize(annotation_data, (CROP_HEIGHT, CROP_WIDTH), method=tf.image.ResizeMethod.NEAREST_NEIGHBOR)
-                    image_data = tf.image.resize_with_crop_or_pad(image_data, long_side, long_side)
-                    image_data = tf.image.resize(image_data, (CROP_HEIGHT, CROP_WIDTH), method=tf.image.ResizeMethod.NEAREST_NEIGHBOR)
+                # Resizeing data
+                X, Y = image_data.shape[0], image_data.shape[1]
+                if X < Y:
+                    long_side = Y
+                else:
+                    long_side = X
+                annotation_data = tf.image.resize_with_crop_or_pad(annotation_data, long_side, long_side)
+                annotation_data = tf.image.resize(annotation_data, (CROP_HEIGHT, CROP_WIDTH), method=tf.image.ResizeMethod.NEAREST_NEIGHBOR)
+                image_data = tf.image.resize_with_crop_or_pad(image_data, long_side, long_side)
+                image_data = tf.image.resize(image_data, (CROP_HEIGHT, CROP_WIDTH), method=tf.image.ResizeMethod.NEAREST_NEIGHBOR)
 
-                    # Reshape annotation_data
-                    annotation_data = tf.reshape(annotation_data, annotation_data.shape[:2])
+                # Reshape annotation_data
+                annotation_data = tf.reshape(annotation_data, annotation_data.shape[:2])
+                annotation_array = annotation_data.numpy()
+                image_array = image_data.numpy()
+                hist, bin = np.histogram(annotation_array, bins=np.arange(LABELS + 1))
+                label_balance_array_resize += hist
 
-                    # Convert ndarray to raw format                
-                    annotation_data = annotation_data.numpy().tobytes()
-                    image_data = image_data.numpy().tobytes()
+                image_array_list.append(image_array)
+                annotation_array_list.append(annotation_array)
 
-                    # Write to Record
-                    feature = {
-                        "image": tf.train.Feature(bytes_list=tf.train.BytesList(value=[image_data])),
-                        "annotation": tf.train.Feature(bytes_list=tf.train.BytesList(value=[annotation_data]))
-                    }
-                    example = tf.train.Example(features=tf.train.Features(feature=feature))
-                    writer.write(example.SerializeToString())
+                status = int((i + 1) / data_num * 100)
+                if status % 2 == 0:
+                    if show_flag == 0:
+                        show_flag = 1
+                        print("\033[F", int(status), "%|", "■" * int(status / 2))  
+                else:
+                    show_flag = 0
 
-                    status = int((i + 1) / data_num * 100)
-                    if status % 2 == 0:
-                        if show_flag == 0:
-                            show_flag = 1
-                            print("\033[F", int(status), "%|", "■" * int(status / 2))  
-                    else:
-                        show_flag = 0
+        annotation_data = annotation_array
+        annotation_image = Image.fromarray(annotation_data)
+        annotation_image.putpalette(palette)
+        annotation_image.show()
 
+        annotation_data = np.array(annotation_array_list, dtype=np.uint8)
+        image_data = np.array(image_array_list, dtype=np.uint8)
+        np.savez(TRAINVAL_RECORDS,
+                 image=image_data,
+                 annotation=annotation_data,
+                 label_pix_resize=label_balance_array_resize,
+                 label_pix=label_balance_array)
+        print("Labels               :", bin)
+        print("Pixels in each label :", label_balance_array)
+        print("Pixels in each label(with resized images) :", label_balance_array)
         print("\n\nDone")
+
     except:
         traceback.print_exc()
         sys.exit(2)
-
 
     # Generate Validate Data Records
     try:
         print("\n\n\nGenerate Validate Data Records...\n")
-        with tf.io.TFRecordWriter(VAL_RECORDS) as writer:
-            with open(VAL_DATA_NAME) as f:
-                reader = csv.reader(f)
-                show_flag = 0
-                data_num = 1448
-                for i, name in enumerate(reader):
-                    # Loading image-like data
-                    image_file_name = image_file_directories["image"] + name[0] + ".jpg"
-                    annotation_file_name = image_file_directories["annotation"] + name[0] + ".png"
-                    with Image.open(image_file_name) as image_object:
-                        image_data = np.array(image_object, dtype=np.uint8)
-                    with Image.open(annotation_file_name) as annotation_object:
-                        annotation_data = np.array(annotation_object, dtype=np.uint8)
-                        annotation_data = annotation_data.reshape(annotation_data.shape + (1,))
-                        annotation_data[annotation_data == 255] = 0
-                        if i == 0:
-                            palette = np.array(annotation_object.getpalette(), dtype=np.uint8).reshape(-1, 3)
+        with open(VAL_DATA_NAME) as f:
+            reader = csv.reader(f)
+            show_flag = 0
+            data_num = 1448
+            for i, name in enumerate(reader):
+                # Loading image-like data
+                image_file_name = image_file_directories["image"] + name[0] + ".jpg"
+                annotation_file_name = image_file_directories["annotation"] + name[0] + ".png"
+                with Image.open(image_file_name) as image_object:
+                    image_data = np.array(image_object, dtype=np.uint8)
+                with Image.open(annotation_file_name) as annotation_object:
+                    annotation_data = np.array(annotation_object, dtype=np.uint8)
+                    annotation_data = annotation_data.reshape(annotation_data.shape + (1,))
+                    annotation_data[annotation_data == 255] = 0
+                    hist, bin = np.histogram(annotation_data, bins=np.arange(LABELS + 1))
+                    label_balance_array += hist
+                    if i == 0:
+                        palette = np.array(annotation_object.getpalette(), dtype=np.uint8).reshape(-1, 3)
 
-                    # Resizeing data
-                    X, Y = image_data.shape[0], image_data.shape[1]
-                    if X < Y:
-                        long_side = Y
-                    else:
-                        long_side = X
-                    annotation_data = tf.image.resize_with_crop_or_pad(annotation_data, long_side, long_side)
-                    annotation_data = tf.image.resize(annotation_data, (CROP_HEIGHT, CROP_WIDTH), method=tf.image.ResizeMethod.NEAREST_NEIGHBOR)
-                    image_data = tf.image.resize_with_crop_or_pad(image_data, long_side, long_side)
-                    image_data = tf.image.resize(image_data, (CROP_HEIGHT, CROP_WIDTH), method=tf.image.ResizeMethod.NEAREST_NEIGHBOR)
+                # Resizeing data
+                X, Y = image_data.shape[0], image_data.shape[1]
+                if X < Y:
+                    long_side = Y
+                else:
+                    long_side = X
+                annotation_data = tf.image.resize_with_crop_or_pad(annotation_data, long_side, long_side)
+                annotation_data = tf.image.resize(annotation_data, (CROP_HEIGHT, CROP_WIDTH), method=tf.image.ResizeMethod.NEAREST_NEIGHBOR)
+                image_data = tf.image.resize_with_crop_or_pad(image_data, long_side, long_side)
+                image_data = tf.image.resize(image_data, (CROP_HEIGHT, CROP_WIDTH), method=tf.image.ResizeMethod.NEAREST_NEIGHBOR)
 
-                    # Reshape annotation_data
-                    annotation_data = tf.reshape(annotation_data, annotation_data.shape[:2])
+                # Reshape annotation_data
+                annotation_data = tf.reshape(annotation_data, annotation_data.shape[:2])
+                annotation_array = annotation_data.numpy()
+                image_array = image_data.numpy()
+                hist, bin = np.histogram(annotation_array, bins=np.arange(LABELS + 1))
+                label_balance_array_resize += hist
 
-                    # Convert ndarray to raw format                
-                    annotation_data = annotation_data.numpy().tobytes()
-                    image_data = image_data.numpy().tobytes()
+                image_array_list.append(image_array)
+                annotation_array_list.append(annotation_array)
 
-                    # Write to Record
-                    feature = {
-                        "image": tf.train.Feature(bytes_list=tf.train.BytesList(value=[image_data])),
-                        "annotation": tf.train.Feature(bytes_list=tf.train.BytesList(value=[annotation_data]))
-                    }
-                    example = tf.train.Example(features=tf.train.Features(feature=feature))
-                    writer.write(example.SerializeToString())
+                status = int((i + 1) / data_num * 100)
+                if status % 2 == 0:
+                    if show_flag == 0:
+                        show_flag = 1
+                        print("\033[F", int(status), "%|", "■" * int(status / 2))  
+                else:
+                    show_flag = 0
 
-                    status = int((i + 1) / data_num * 100)
-                    if status % 2 == 0:
-                        if show_flag == 0:
-                            show_flag = 1
-                            print("\033[F", int(status), "%|", "■" * int(status / 2))  
-                    else:
-                        show_flag = 0
+        annotation_data = annotation_array
+        annotation_image = Image.fromarray(annotation_data)
+        annotation_image.putpalette(palette)
+        annotation_image.show()
 
+        annotation_data = np.array(annotation_array_list, dtype=np.uint8)
+        image_data = np.array(image_array_list, dtype=np.uint8)
+        np.savez(VAL_RECORDS,
+                 image=image_data,
+                 annotation=annotation_data,
+                 label_pix_resize=label_balance_array_resize,
+                 label_pix=label_balance_array)
+        print("Labels               :", bin)
+        print("Pixels in each label :", label_balance_array)
+        print("Pixels in each label(with resized images) :", label_balance_array)
         print("\n\nDone")
+
     except:
         traceback.print_exc()
-        sys.exit(2)
-
+        sys.exit(3)
 
     print("\n\n\nAll Resizing Done!!!")
 
