a5c0460ae687227b83241cc8c03ac28d68f5cade
shtsno24 <shtsno.24@gmail.com>
2020-02-28 17:32:01 +0900

    wip : Train

--- VOC2012/Test.py
+++ VOC2012/Test.py
@@ -17,14 +17,14 @@ def weighted_SparseCategoricalCrossentropy(classes):
 
 try:
 
-    # TEST_IMAGE = "./data/JPEGImages/2007_004830.jpg"
-    # TEST_ANNOTATION = "./data/SegmentationClass/2007_004830.png"
-    TEST_IMAGE = "./data/JPEGImages/2007_000032.jpg"
-    TEST_ANNOTATION = "./data/SegmentationClass/2007_000032.png"
+    TEST_IMAGE = "./data/JPEGImages/2007_004830.jpg"
+    TEST_ANNOTATION = "./data/SegmentationClass/2007_004830.png"
+    # TEST_IMAGE = "./data/JPEGImages/2007_000032.jpg"
+    # TEST_ANNOTATION = "./data/SegmentationClass/2007_000032.png"
     MODEL_FILE = "TestNet_VOC2012_npz.h5"
     LABELS = 21
     COLOR_DEPTH = 3
-    CROP_HEIGHT = 128 # sensor.LCD[128, 160]
+    CROP_HEIGHT = 128  # sensor.LCD[128, 160]
     CROP_WIDTH = 160
 
     # Load data
