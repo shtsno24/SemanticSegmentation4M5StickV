6f6cd7a0d4fc5087d3244880da36907860bd4d02
masudalab <masudalab>
2020-03-25 06:53:09 +0900

    Train model

--- VOC2012/Model_V0_1.py
+++ VOC2012/Model_V0_1.py
@@ -19,221 +19,231 @@ def weighted_SparseCategoricalCrossentropy(sample_weights, classes=5):
     return loss_function
 
 
-def Multiscale_Depthwise_Conv(x, out_depth, internal_scale=4, Momentum=0.1, Droprate=0.01, Alpha=0.1, Activation_Func=True):
-    internal_depth = int(out_depth / internal_scale)
+def initial_block(x, input_depth, channel, stride=(2, 2), Momentum=0.1):
+    # x_conv = Conv2D(channel - input_depth, kernel_size, padding="same", strides=stride)(x)
+    internal_channel = int((channel - input_depth) / 3)
 
-    x = Conv2D(internal_depth, (1, 1))(x)
-    x = ReLU()(x)
+    x_conv_3 = ZeroPadding2D(padding=((1, 1), (1, 1)))(x)
+    x_conv_3 = DepthwiseConv2D((3, 3))(x_conv_3)
+    x_conv_3 = Activation("relu")(x_conv_3)
+    x_conv_3 = Conv2D(internal_channel, (1, 1))(x_conv_3)
+    x_conv_3 = Activation("relu")(x_conv_3)
 
-    x_3 = ZeroPadding2D(padding=((1, 1), (1, 1)))(x)
-    x_3 = DepthwiseConv2D((3, 3))(x_3)
-    x_3 = BatchNormalization(momentum=Momentum)(x_3)
-    x_3 = ReLU()(x_3)
+    x_conv_5 = ZeroPadding2D(padding=((1, 1), (1, 1)))(x_conv_3)
+    x_conv_5 = DepthwiseConv2D((3, 3))(x_conv_5)
+    x_conv_5 = Activation("relu")(x_conv_5)
+    x_conv_5 = Conv2D(internal_channel, (1, 1))(x_conv_5)
+    x_conv_5 = Activation("relu")(x_conv_5)
 
-    x_5 = ZeroPadding2D(padding=((1, 1), (1, 1)))(x_3)
-    x_5 = DepthwiseConv2D((3, 3))(x_5)
-    x_5 = BatchNormalization(momentum=Momentum)(x_5)
-    x_5 = ReLU()(x_5)
+    x_conv_7 = ZeroPadding2D(padding=((1, 1), (1, 1)))(x_conv_5)
+    x_conv_7 = DepthwiseConv2D((3, 3))(x_conv_7)
+    x_conv_7 = Activation("relu")(x_conv_7)
+    x_conv_7 = Conv2D(internal_channel, (1, 1))(x_conv_7)
+    x_conv_7 = Activation("relu")(x_conv_7)
 
-    x_7 = ZeroPadding2D(padding=((1, 1), (1, 1)))(x_5)
-    x_7 = DepthwiseConv2D((3, 3))(x_7)
-    x_7 = BatchNormalization(momentum=Momentum)(x_7)
-    x_7 = ReLU()(x_7)
+    x_conv = Concatenate(axis=3)([x_conv_3, x_conv_5, x_conv_7])
+    x_conv = MaxPooling2D(pool_size=(2, 2))(x_conv)
 
-    x = MaxPooling2D(pool_size=(2, 2))(x)
-    x = UpSampling2D(size=(2, 2))(x)
+    x_pool = MaxPooling2D(pool_size=(2, 2))(x)
 
-    x = Concatenate(axis=3)([x, x_3, x_5, x_7])
-    x = Conv2D(out_depth, (1, 1))(x)
+    y = Concatenate(axis=3)([x_conv, x_pool])
+    y = Conv2D(channel, (1, 1))(y)
+    y = BatchNormalization(momentum=Momentum)(y)
+    y = Activation("relu")(y)
+
+    return y
+
+
+def bottleneck_downsample(x, output_depth, internal_scale=4, Momentum=0.1):
+    internal_depth = int(output_depth / internal_scale)
+
+    x_conv = Conv2D(internal_depth, (1, 1))(x)
+    x_conv = BatchNormalization(momentum=Momentum)(x_conv)
+    x_conv = Activation("relu")(x_conv)
+    x_conv = DepthwiseConv2D((2, 2), strides=(2, 2))(x)
+    x_conv = BatchNormalization(momentum=Momentum)(x_conv)
+    x_conv = Activation("relu")(x_conv)
+
+    x_conv = ZeroPadding2D(padding=((1, 1), (1, 1)))(x_conv)
+    x_conv = DepthwiseConv2D((3, 3))(x_conv)
+    x_conv = BatchNormalization(momentum=Momentum)(x_conv)
+    x_conv = Activation("relu")(x_conv)
+    x_conv = Conv2D(internal_depth, (1, 1))(x_conv)
+    x_conv = BatchNormalization(momentum=Momentum)(x_conv)
+    x_conv = Activation("relu")(x_conv)
+
+    x_conv = Conv2D(output_depth, (1, 1), use_bias=False)(x_conv)
+    x_conv = BatchNormalization(momentum=Momentum)(x_conv)
+    x_conv = SpatialDropout2D(0.01)(x_conv)
+
+    x_pool = MaxPooling2D(pool_size=(2, 2))(x)
+
+    x = Concatenate(axis=3)([x_conv, x_pool])
+    x = Conv2D(output_depth, (1, 1))(x)
     x = BatchNormalization(momentum=Momentum)(x)
-    x = SpatialDropout2D(Droprate)(x)
-    if Activation_Func == True:
-        x = ReLU()(x)
-
-    return x
+    y = Activation("relu")(x)
+    return y
 
 
-def Multiscale_Depthwise_Conv_Downsize(x, out_depth, internal_scale=4, Momentum=0.1, Droprate=0.01, Alpha=0.1, Activation_Func=True):
-    internal_depth = int(out_depth / internal_scale)
+def bottleneck(x, output_depth, internal_scale=4, Momentum=0.1):
+    internal_depth = int(output_depth / internal_scale)
 
-    x = Conv2D(internal_depth, (1, 1))(x)
-    x = ReLU()(x)
+    x_conv = Conv2D(internal_depth, (1, 1))(x)
+    x_conv = BatchNormalization(momentum=Momentum)(x_conv)
+    x_conv = Activation("relu")(x_conv)
 
-    x_3 = ZeroPadding2D(padding=((1, 1), (1, 1)))(x)
-    x_3 = DepthwiseConv2D((3, 3))(x_3)
-    x_3 = BatchNormalization(momentum=Momentum)(x_3)
-    x_3 = ReLU()(x_3)
+    x_conv = ZeroPadding2D(padding=((1, 1), (1, 1)))(x_conv)
+    x_conv = DepthwiseConv2D((3, 3))(x_conv)
+    x_conv = BatchNormalization(momentum=Momentum)(x_conv)
+    x_conv = Activation("relu")(x_conv)
+    x_conv = Conv2D(internal_depth, (1, 1))(x_conv)
+    x_conv = BatchNormalization(momentum=Momentum)(x_conv)
+    x_conv = Activation("relu")(x_conv)
 
-    x_5 = ZeroPadding2D(padding=((1, 1), (1, 1)))(x_3)
-    x_5 = DepthwiseConv2D((3, 3))(x_5)
-    x_5 = BatchNormalization(momentum=Momentum)(x_5)
-    x_5 = ReLU()(x_5)
+    x_conv = Conv2D(output_depth, (1, 1), use_bias=False)(x_conv)
+    x_conv = BatchNormalization(momentum=Momentum)(x_conv)
+    x_conv = SpatialDropout2D(0.01)(x_conv)
 
-    x_7 = ZeroPadding2D(padding=((1, 1), (1, 1)))(x_5)
-    x_7 = DepthwiseConv2D((3, 3))(x_7)
-    x_7 = BatchNormalization(momentum=Momentum)(x_7)
-    x_7 = ReLU()(x_7)
+    x_pool = MaxPooling2D(pool_size=(2, 2))(x)
+    x_pool = UpSampling2D(size=(2, 2))(x_pool)
 
-    x = Concatenate(axis=3)([x, x_3, x_5, x_7])
-    x = MaxPooling2D(pool_size=(2, 2))(x)
-    x = Conv2D(out_depth, (1, 1))(x)
+    x = Concatenate()([x_conv, x_pool])
+    x = Conv2D(output_depth, (1, 1))(x)
     x = BatchNormalization(momentum=Momentum)(x)
-    x = SpatialDropout2D(Droprate)(x)
-    x = ReLU()(x)
-
-    return x
+    y = Activation("relu")(x)
+    return y
 
 
-def Multiscale_Depthwise_Conv_Upsize(x, out_depth, internal_scale=4, Momentum=0.1, Droprate=0.01, Alpha=0.1, Activation_Func=True):
-    internal_depth = int(out_depth / internal_scale)
+def bottleneck_asymmetric(x, asymmetric, output_depth, internal_scale=4, Momentum=0.1):
+    internal_depth = int(output_depth / internal_scale)
+    pad_half = int(asymmetric / 2)
 
-    x = Conv2D(internal_depth, (1, 1))(x)
-    x = ReLU()(x)
+    x_conv = Conv2D(internal_depth, (1, 1))(x)
+    x_conv = BatchNormalization(momentum=Momentum)(x_conv)
+    x_conv = Activation("relu")(x_conv)
 
-    x_3 = ZeroPadding2D(padding=((1, 1), (1, 1)))(x)
-    x_3 = DepthwiseConv2D((3, 3))(x_3)
-    x_3 = BatchNormalization(momentum=Momentum)(x_3)
-    x_3 = ReLU()(x_3)
+    x_conv = ZeroPadding2D(padding=((pad_half, pad_half), (pad_half, pad_half)))(x_conv)
+    x_conv = Conv2D(internal_depth, (1, asymmetric), use_bias=False)(x_conv)
+    x_conv = Conv2D(internal_depth, (asymmetric, 1))(x_conv)
+    x_conv = BatchNormalization(momentum=Momentum)(x_conv)
+    x_conv = Activation("relu")(x_conv)
 
-    x_5 = ZeroPadding2D(padding=((1, 1), (1, 1)))(x_3)
-    x_5 = DepthwiseConv2D((3, 3))(x_5)
-    x_5 = BatchNormalization(momentum=Momentum)(x_5)
-    x_5 = ReLU()(x_5)
+    x_conv = Conv2D(output_depth, (1, 1), use_bias=False)(x_conv)
+    x_conv = BatchNormalization(momentum=Momentum)(x_conv)
+    x_conv = SpatialDropout2D(0.01)(x_conv)
 
-    x_7 = ZeroPadding2D(padding=((1, 1), (1, 1)))(x_5)
-    x_7 = DepthwiseConv2D((3, 3))(x_7)
-    x_7 = BatchNormalization(momentum=Momentum)(x_7)
-    x_7 = ReLU()(x_7)
+    x_pool = MaxPooling2D(pool_size=(2, 2))(x)
+    x_pool = UpSampling2D(size=(2, 2))(x_pool)
 
-    x = Concatenate(axis=3)([x, x_3, x_5, x_7])
-    x = UpSampling2D(size=(2, 2))(x)
-
-    x = ZeroPadding2D(padding=((1, 1), (1, 1)))(x)
-    x = DepthwiseConv2D((3, 3))(x)
-    x = Conv2D(out_depth, (1, 1))(x)
+    x = Concatenate()([x_conv, x_pool])
+    x = Conv2D(output_depth, (1, 1))(x)
     x = BatchNormalization(momentum=Momentum)(x)
-    x = SpatialDropout2D(Droprate)(x)
-    x = ReLU()(x)
-
-    return x
+    y = Activation("relu")(x)
+    return y
 
 
-"""
-def TestNet(input_shape=(128, 160, 3), classes=21):
-    inputs = Input(shape=input_shape)
+def bottleneck_dilated(x, dilated, output_depth, internal_scale=4, Momentum=0.1):
+    internal_depth = int(output_depth / internal_scale)
 
-    x = Multiscale_Depthwise_Conv_Downsize(inputs, 32)
-    x_0 = x
-    # 64 x 80 x 32
+    x_conv = Conv2D(internal_depth, (1, 1))(x)
+    x_conv = BatchNormalization(momentum=Momentum)(x_conv)
+    x_conv = Activation("relu")(x_conv)
 
-    for _ in range(2):
-        x = Multiscale_Depthwise_Conv(x, 64)
-    x = Multiscale_Depthwise_Conv_Downsize(x, 128)
-    x_1 = x
-    # 32 x 40 x 128
+    x_conv = ZeroPadding2D(padding=((dilated, dilated), (dilated, dilated)))(x_conv)
+    x_conv = DepthwiseConv2D((3, 3), dilation_rate=(dilated, dilated))(x_conv)
+    x_conv = BatchNormalization(momentum=Momentum)(x_conv)
+    x_conv = Activation("relu")(x_conv)
+    x_conv = Conv2D(internal_depth, (1, 1))(x_conv)
+    x_conv = BatchNormalization(momentum=Momentum)(x_conv)
+    x_conv = Activation("relu")(x_conv)
+    # dilation_rate = 2 : padding = 2
+    # dilation_rate = 4 : padding = 4
+    # dilation_rate = 8 : padding = 8
+    # dilation_rate = 16: padding = 16
 
-    for _ in range(2):
-        x = Multiscale_Depthwise_Conv(x, 128, internal_scale=8)
-    x = Multiscale_Depthwise_Conv_Downsize(x, 256)
-    x_2 = x
-    # 16 x 20 x 256
+    x_conv = Conv2D(output_depth, (1, 1), use_bias=False)(x_conv)
+    x_conv = BatchNormalization(momentum=Momentum)(x_conv)
+    x_conv = SpatialDropout2D(0.01)(x_conv)
 
-    for _ in range(1):
-        x = Multiscale_Depthwise_Conv(x, 256, internal_scale=8)
-    x = Multiscale_Depthwise_Conv_Downsize(x, 256)
-    x_3 = x
-    # 8 x 10 x 256
+    x_pool = MaxPooling2D(pool_size=(2, 2))(x)
+    x_pool = UpSampling2D(size=(2, 2))(x_pool)
 
-    for _ in range(1):
-        x = Multiscale_Depthwise_Conv(x, 256, internal_scale=8)
-    x = Multiscale_Depthwise_Conv_Downsize(x, 256)
-    x_4 = x
-    # 4 x 5 x 512
+    x = Concatenate()([x_conv, x_pool])
+    x = Conv2D(output_depth, (1, 1))(x)
+    x = BatchNormalization(momentum=Momentum)(x)
+    y = Activation("relu")(x)
+    return y
 
-    x = Multiscale_Concat(x_0, x_1, x_2, x_3, x_4, 256)
 
-    for _ in range(2):
-        x = Multiscale_Depthwise_Conv(x, 256, internal_scale=8)
+def bottleneck_upsampling(x, output_depth, internal_scale=4, Momentum=0.1):
+    internal_depth = int(output_depth / internal_scale)
 
-    x = Concatenate(axis=3)([x, x_2])
-    x = Multiscale_Depthwise_Conv_Upsize(x, 128)
+    x_conv = Conv2D(internal_depth, (1, 1))(x)
+    x_conv = BatchNormalization(momentum=Momentum)(x_conv)
+    x_conv = Activation("relu")(x_conv)
+    x_conv = Conv2DTranspose(internal_depth, (3, 3), strides=(2, 2), padding="same")(x_conv)
+    x_conv = BatchNormalization(momentum=Momentum)(x_conv)
+    x_conv = Activation("relu")(x_conv)
+    x_conv = Conv2D(output_depth, (1, 1), use_bias=False)(x_conv)
+    x_conv = BatchNormalization(momentum=Momentum)(x_conv)
+    x_conv = SpatialDropout2D(0.01)(x_conv)
 
-    # 32 x 40 x 128
-    for _ in range(1):
-        x = Multiscale_Depthwise_Conv(x, 128)
+    x_pool = Conv2D(output_depth, (1, 1), use_bias=False)(x)
+    x_conv = BatchNormalization(momentum=Momentum)(x_conv)
+    x_pool = UpSampling2D(size=(2, 2))(x_pool)
 
-    x = Concatenate(axis=3)([x, x_1])
-    x = Multiscale_Depthwise_Conv_Upsize(x, 64)
-    # 64 x 80 x 64
-    for _ in range(1):
-        x = Multiscale_Depthwise_Conv(x, 64)
-
-    x = Concatenate(axis=3)([x, x_0])
-    x = Multiscale_Depthwise_Conv_Upsize(x, 32)
-    # 128 x 160 x 32
-    x = Multiscale_Depthwise_Conv(x, classes)
-
-    # 128 x 160 x classes
-    outputs = Softmax()(x)
-
-    model = Model(inputs, outputs)
-    return model
-"""
+    x = Concatenate()([x_conv, x_pool])
+    x = Conv2D(output_depth, (1, 1))(x)
+    x = BatchNormalization(momentum=Momentum)(x)
+    y = Activation("relu")(x)
+    return y
 
 
 def TestNet(input_shape=(128, 128, 3), classes=5):
+
     inputs = Input(shape=input_shape)
+    x = initial_block(inputs, 4, 16)
+    x_skip_0 = x
+    # 64 x 64 x 16
 
-    # x = MaxPooling2D(pool_size=(4, 4))(inputs)
+    x = bottleneck_downsample(x, 64)
 
-    x_2 = Multiscale_Depthwise_Conv_Downsize(inputs, 16)
-    x_2 = Multiscale_Depthwise_Conv(x_2, 16)
+    # 32 x 32 x 64
 
-    x_4 = Multiscale_Depthwise_Conv_Downsize(x_2, 32)
-    x_4 = Multiscale_Depthwise_Conv(x_4, 32)
-    x_4 = Multiscale_Depthwise_Conv(x_4, 32)
+    for _ in range(4):
+        x = bottleneck(x, 64)
+    x_skip_1 = x
+    x = bottleneck_downsample(x, 128)
 
-    x_8 = Multiscale_Depthwise_Conv_Downsize(x_4, 64)
-    x_8 = Multiscale_Depthwise_Conv(x_8, 64)
-    x_8 = Multiscale_Depthwise_Conv(x_8, 64)
-    # x_8 = Multiscale_Depthwise_Conv(x_8, 64)
+    # 16 x 16 x 128
 
-    x_16 = Multiscale_Depthwise_Conv_Downsize(x_8, 64)
-    x_16 = Multiscale_Depthwise_Conv(x_16, 64)
-    x_16 = Multiscale_Depthwise_Conv(x_16, 64)
-    # x_16 = Multiscale_Depthwise_Conv(x_16, 128)
+    for _ in range(2):
+        x = bottleneck(x, 128)
+        x = bottleneck_dilated(x, 2, 128)
+        x = bottleneck_asymmetric(x, 5, 128)
+        x = bottleneck_dilated(x, 4, 128)
+        x = bottleneck(x, 128)
+        x = bottleneck_dilated(x, 8, 128)
+        x = bottleneck_asymmetric(x, 5, 128)
+        x = bottleneck_dilated(x, 16, 128)
 
-    # x_2 = Conv2D(16, (1, 1))(x_2)
-    # x_2 = BatchNormalization(momentum=0.1)(x_2)
-    # x_2 = ReLU()(x_2)
-    # x_2 = MaxPooling2D(pool_size=(2, 2))(x_2)
+    x = bottleneck_upsampling(x, 64)
+    x = Concatenate(axis=3)([x, x_skip_1])
+    x = bottleneck(x, 64)
+    x = bottleneck(x, 64)
 
-    x_4 = Conv2D(16, (1, 1))(x_4)
-    x_4 = BatchNormalization(momentum=0.1)(x_4)
-    x_4 = ReLU()(x_4)
-    x_4 = UpSampling2D(size=(2, 2))(x_4)
+    # 32 x 32 x 64
 
-    x_8 = Conv2D(16, (1, 1))(x_8)
-    x_8 = BatchNormalization(momentum=0.1)(x_8)
-    x_8 = ReLU()(x_8)
-    x_8 = UpSampling2D(size=(4, 4))(x_8)
+    x = bottleneck_upsampling(x, 16)
+    x = Concatenate(axis=3)([x, x_skip_0])
+    x = bottleneck(x, 16)
 
-    x_16 = Conv2D(16, (1, 1))(x_16)
-    x_16 = BatchNormalization(momentum=0.1)(x_16)
-    x_16 = ReLU()(x_16)
-    x_16 = UpSampling2D(size=(8, 8))(x_16)
+    # 64 x 64 x 16
 
-    x = Concatenate(axis=3)([x_2, x_4, x_8, x_16])
-    x = Multiscale_Depthwise_Conv(x, 32)
-    x = Multiscale_Depthwise_Conv(x, 16)
+    x = Conv2DTranspose(classes, kernel_size=(2, 2), strides=(2, 2), padding="same")(x)
 
-    x = UpSampling2D(size=(2, 2))(x)
-    x = Multiscale_Depthwise_Conv(x, classes, Activation_Func=False)
+    # 128 x 128 x classes
 
-    outputs = Softmax()(x)
-    # outputs = UpSampling2D(size=(2, 2), interpolation='bilinear')(outputs)
-
+    outputs = Activation("softmax")(x)
     model = Model(inputs, outputs)
     return model
-
-    # 2097152 B
