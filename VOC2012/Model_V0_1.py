fa8d7e3ad59cde9ae6caec26de9df32b10b33a83
shtsno24 <shtsno24@gmail.com>
2020-04-16 22:18:11 +0900

    change model

--- VOC2012/Model_V0_1.py
+++ VOC2012/Model_V0_1.py
@@ -19,234 +19,107 @@ def weighted_SparseCategoricalCrossentropy(sample_weights, classes=5):
     return loss_function
 
 
-def initial_block(x, input_depth, channel, stride=(2, 2), Momentum=0.1):
-    # x_conv = Conv2D(channel - input_depth, kernel_size, padding="same", strides=stride)(x)
-    internal_channel = int((channel - input_depth) / 3)
+def SkipConvBlock(x, output_channel, kernel_size, padding, internal_mag=4, momentum=0.1, drop_rate=0.01):
+    internal_channel = int(output_channel / internal_mag) * (internal_mag - 1)
+    skip_channel = output_channel - internal_channel
 
-    x_conv_3 = ZeroPadding2D(padding=((1, 1), (1, 1)))(x)
-    x_conv_3 = DepthwiseConv2D((3, 3))(x_conv_3)
-    x_conv_3 = Activation("relu")(x_conv_3)
-    x_conv_3 = Conv2D(internal_channel, (1, 1))(x_conv_3)
-    x_conv_3 = Activation("relu")(x_conv_3)
+    C = Conv2D(internal_channel, (1, 1))(x)
+    C = BatchNormalization(momentum=momentum)(x)
+    C = ReLU()(C)
+    C = ZeroPadding2D(padding=padding)(C)
+    C = DepthwiseConv2D(kernel_size)(C)
+    C = BatchNormalization(momentum=momentum)(C)
+    C = ReLU()(C)
+    C = Conv2D(output_channel, (1, 1))(x)
+    C = BatchNormalization(momentum=momentum)(C)
+    C = ReLU()(C)
 
-    x_conv_5 = ZeroPadding2D(padding=((1, 1), (1, 1)))(x_conv_3)
-    x_conv_5 = DepthwiseConv2D((3, 3))(x_conv_5)
-    x_conv_5 = Activation("relu")(x_conv_5)
-    x_conv_5 = Conv2D(internal_channel, (1, 1))(x_conv_5)
-    x_conv_5 = Activation("relu")(x_conv_5)
+    Skip = Conv2D(skip_channel, (1, 1))(x)
+    Skip = BatchNormalization(momentum=momentum)(Skip)
 
-    # x_conv_7 = ZeroPadding2D(padding=((1, 1), (1, 1)))(x_conv_5)
-    # x_conv_7 = DepthwiseConv2D((3, 3))(x_conv_7)
-    # x_conv_7 = Activation("relu")(x_conv_7)
-    # x_conv_7 = Conv2D(internal_channel, (1, 1))(x_conv_7)
-    # x_conv_7 = Activation("relu")(x_conv_7)
+    x = Concatenate(axis=3)([Skip, C])
+    x = Conv2D(output_channel, (1, 1))(x)
+    x = BatchNormalization(momentum=momentum)(x)
+    x = ReLU()(x)
 
-    # x_conv = Concatenate(axis=3)([x_conv_3, x_conv_5, x_conv_7])
-    x_conv = Concatenate(axis=3)([x_conv_3, x_conv_5])
-    x_conv = MaxPooling2D(pool_size=(2, 2))(x_conv)
-
-    x_pool = MaxPooling2D(pool_size=(2, 2))(x)
-
-    y = Concatenate(axis=3)([x_conv, x_pool])
-    y = Conv2D(channel, (1, 1))(y)
-    y = BatchNormalization(momentum=Momentum)(y)
-    y = Activation("relu")(y)
-
-    return y
+    return x
 
 
-def bottleneck(x, output_depth, internal_scale=4, Momentum=0.1):
-    internal_depth = int(output_depth / internal_scale)
+def SkipConvBlockD(x, output_channel, kernel_size, padding, internal_mag=4, momentum=0.1, drop_rate=0.01):
+    internal_channel = int(output_channel / internal_mag) * (internal_mag - 1)
+    skip_channel = output_channel - internal_channel
 
-    x_conv = Conv2D(internal_depth, (1, 1))(x)
-    x_conv = BatchNormalization(momentum=Momentum)(x_conv)
-    x_conv = Activation("relu")(x_conv)
+    C = Conv2D(internal_channel, (1, 1))(x)
+    C = BatchNormalization(momentum=momentum)(x)
+    C = ReLU()(C)
+    C = ZeroPadding2D(padding=padding)(C)
+    C = DepthwiseConv2D(kernel_size)(C)
+    C = BatchNormalization(momentum=momentum)(C)
+    C = ReLU()(C)
+    C = Conv2D(output_channel, (1, 1))(x)
+    C = BatchNormalization(momentum=momentum)(C)
+    C = ReLU()(C)
 
-    for _ in range(2):
-        x_conv = ZeroPadding2D(padding=((1, 1), (1, 1)))(x_conv)
-        x_conv = DepthwiseConv2D((3, 3))(x_conv)
-        x_conv = BatchNormalization(momentum=Momentum)(x_conv)
-        x_conv = Activation("relu")(x_conv)
-        x_conv = Conv2D(internal_depth, (1, 1))(x_conv)
-        x_conv = BatchNormalization(momentum=Momentum)(x_conv)
-        x_conv = Activation("relu")(x_conv)
+    Skip = Conv2D(skip_channel, (1, 1))(x)
+    Skip = BatchNormalization(momentum=momentum)(Skip)
 
-    x_conv = Conv2D(output_depth, (1, 1))(x_conv)
-    x_conv = BatchNormalization(momentum=Momentum)(x_conv)
-    x_conv = SpatialDropout2D(0.01)(x_conv)
+    x = Concatenate(axis=3)([Skip, C])
+    x = ZeroPadding2D(padding=(1, 1))(x)
+    x = DepthwiseConv2D((3, 3), strides=(2, 2))(x)
+    x = BatchNormalization(momentum=momentum)(x)
+    x = ReLU()(x)
 
-    x_pool = MaxPooling2D(pool_size=(2, 2))(x)
-    x_pool = UpSampling2D(size=(2, 2))(x_pool)
-
-    x = Concatenate()([x_conv, x_pool])
-    x = Conv2D(output_depth, (1, 1))(x)
-    x = BatchNormalization(momentum=Momentum)(x)
-    y = Activation("relu")(x)
-    return y
+    return x
 
 
-def bottleneck_downsample(x, output_depth, internal_scale=4, Momentum=0.1):
-    internal_depth = int(output_depth / internal_scale)
+def SkipConvBlockU(x, output_channel, kernel_size, padding, activation=True, internal_mag=4, momentum=0.1, drop_rate=0.01):
+    internal_channel = int(output_channel / internal_mag) * (internal_mag - 1)
+    skip_channel = output_channel - internal_channel
 
-    x_conv = Conv2D(internal_depth, (1, 1))(x)
-    x_conv = BatchNormalization(momentum=Momentum)(x_conv)
-    x_conv = Activation("relu")(x_conv)
-    x_conv = DepthwiseConv2D((2, 2), strides=(2, 2))(x)
-    x_conv = BatchNormalization(momentum=Momentum)(x_conv)
-    x_conv = Activation("relu")(x_conv)
+    C = Conv2D(internal_channel, (1, 1))(x)
+    C = BatchNormalization(momentum=momentum)(x)
+    C = ReLU()(C)
+    C = ZeroPadding2D(padding=padding)(C)
+    C = DepthwiseConv2D(kernel_size)(C)
+    C = BatchNormalization(momentum=momentum)(C)
+    C = ReLU()(C)
+    C = Conv2D(output_channel, (1, 1))(x)
+    C = BatchNormalization(momentum=momentum)(C)
+    C = ReLU()(C)
 
-    for _ in range(2):
-        x_conv = ZeroPadding2D(padding=((1, 1), (1, 1)))(x_conv)
-        x_conv = DepthwiseConv2D((3, 3))(x_conv)
-        x_conv = BatchNormalization(momentum=Momentum)(x_conv)
-        x_conv = Activation("relu")(x_conv)
-        x_conv = Conv2D(internal_depth, (1, 1))(x_conv)
-        x_conv = BatchNormalization(momentum=Momentum)(x_conv)
-        x_conv = Activation("relu")(x_conv)
+    Skip = Conv2D(skip_channel, (1, 1))(x)
+    Skip = BatchNormalization(momentum=momentum)(Skip)
 
-    x_conv = Conv2D(output_depth, (1, 1))(x_conv)
-    x_conv = BatchNormalization(momentum=Momentum)(x_conv)
-    x_conv = SpatialDropout2D(0.01)(x_conv)
+    x = Concatenate(axis=3)([Skip, C])
+    x = Conv2DTranspose(output_channel, (2, 2), strides=(2, 2))(x)
+    x = BatchNormalization(momentum=momentum)(x)
+    if activation is True:
+        x = ReLU()(x)
 
-    x_pool = MaxPooling2D(pool_size=(2, 2))(x)
-
-    x = Concatenate(axis=3)([x_conv, x_pool])
-    x = Conv2D(output_depth, (1, 1))(x)
-    x = BatchNormalization(momentum=Momentum)(x)
-    y = Activation("relu")(x)
-    return y
-
-
-def bottleneck_asymmetric(x, asymmetric, output_depth, internal_scale=4, Momentum=0.1):
-    internal_depth = int(output_depth / internal_scale)
-    pad_half = int(asymmetric / 2)
-
-    x_conv = Conv2D(internal_depth, (1, 1))(x)
-    x_conv = BatchNormalization(momentum=Momentum)(x_conv)
-    x_conv = Activation("relu")(x_conv)
-
-    x_conv = ZeroPadding2D(padding=((pad_half, pad_half), (pad_half, pad_half)))(x_conv)
-    x_conv = Conv2D(internal_depth, (1, asymmetric))(x_conv)
-    x_conv = Conv2D(internal_depth, (asymmetric, 1))(x_conv)
-    x_conv = BatchNormalization(momentum=Momentum)(x_conv)
-    x_conv = Activation("relu")(x_conv)
-
-    x_conv = Conv2D(output_depth, (1, 1))(x_conv)
-    x_conv = BatchNormalization(momentum=Momentum)(x_conv)
-    x_conv = SpatialDropout2D(0.01)(x_conv)
-
-    x_pool = MaxPooling2D(pool_size=(2, 2))(x)
-    x_pool = UpSampling2D(size=(2, 2))(x_pool)
-
-    x = Concatenate()([x_conv, x_pool])
-    x = Conv2D(output_depth, (1, 1))(x)
-    x = BatchNormalization(momentum=Momentum)(x)
-    y = Activation("relu")(x)
-    return y
-
-
-def bottleneck_dilated(x, dilated, output_depth, internal_scale=4, Momentum=0.1):
-    internal_depth = int(output_depth / internal_scale)
-
-    x_conv = Conv2D(internal_depth, (1, 1))(x)
-    x_conv = BatchNormalization(momentum=Momentum)(x_conv)
-    x_conv = Activation("relu")(x_conv)
-
-    for _ in range(2):
-        # x_conv = ZeroPadding2D(padding=((dilated, dilated), (dilated, dilated)))(x_conv)  # <- can't convert to tflite
-        x_conv = DepthwiseConv2D((3, 3), dilation_rate=(dilated, dilated), padding="same")(x_conv)
-        x_conv = BatchNormalization(momentum=Momentum)(x_conv)
-        x_conv = Activation("relu")(x_conv)
-        x_conv = Conv2D(internal_depth, (1, 1))(x_conv)
-        x_conv = BatchNormalization(momentum=Momentum)(x_conv)
-        x_conv = Activation("relu")(x_conv)
-        # dilation_rate = 2 : padding = 2
-        # dilation_rate = 4 : padding = 4
-        # dilation_rate = 8 : padding = 8
-        # dilation_rate = 16: padding = 16
-
-    x_conv = Conv2D(output_depth, (1, 1))(x_conv)
-    x_conv = BatchNormalization(momentum=Momentum)(x_conv)
-    x_conv = SpatialDropout2D(0.01)(x_conv)
-
-    x_pool = MaxPooling2D(pool_size=(2, 2))(x)
-    x_pool = UpSampling2D(size=(2, 2))(x_pool)
-
-    x = Concatenate()([x_conv, x_pool])
-    x = Conv2D(output_depth, (1, 1))(x)
-    x = BatchNormalization(momentum=Momentum)(x)
-    y = Activation("relu")(x)
-    return y
-
-
-def bottleneck_upsampling(x, output_depth, internal_scale=4, Momentum=0.1):
-    internal_depth = int(output_depth / internal_scale)
-
-    x_conv = Conv2D(internal_depth, (1, 1))(x)
-    x_conv = BatchNormalization(momentum=Momentum)(x_conv)
-    x_conv = Activation("relu")(x_conv)
-    x_conv = Conv2DTranspose(internal_depth, (3, 3), strides=(2, 2), padding="same")(x_conv)
-    x_conv = BatchNormalization(momentum=Momentum)(x_conv)
-    x_conv = Activation("relu")(x_conv)
-    x_conv = Conv2D(output_depth, (1, 1), use_bias=False)(x_conv)
-    x_conv = BatchNormalization(momentum=Momentum)(x_conv)
-    x_conv = SpatialDropout2D(0.01)(x_conv)
-
-    x_pool = Conv2D(output_depth, (1, 1), use_bias=False)(x)
-    x_conv = BatchNormalization(momentum=Momentum)(x_conv)
-    x_pool = UpSampling2D(size=(2, 2))(x_pool)
-
-    x = Concatenate()([x_conv, x_pool])
-    x = Conv2D(output_depth, (1, 1))(x)
-    x = BatchNormalization(momentum=Momentum)(x)
-    y = Activation("relu")(x)
-    return y
+    return x
 
 
 def TestNet(input_shape=(64, 64, 3), classes=5):
-
     inputs = Input(shape=input_shape)
-    x = initial_block(inputs, 4, 16)
-    x_skip_0 = x
-    # 64 x 64 x 16
 
-    x = bottleneck_downsample(x, 64)
-
-    # 32 x 32 x 64
-
-    for _ in range(4):
-        x = bottleneck(x, 64)
-    x_skip_1 = x
-    x = bottleneck_downsample(x, 128)
-
-    # 16 x 16 x 128
-
+    x0 = SkipConvBlockD(inputs, 32, (3, 3), padding=(1, 1), internal_mag=2)
     for _ in range(2):
-        x = bottleneck(x, 128)
-        x = bottleneck_dilated(x, 2, 128)
-        x = bottleneck_asymmetric(x, 5, 128)
-        x = bottleneck_dilated(x, 4, 128)
-        x = bottleneck(x, 128)
-        x = bottleneck_dilated(x, 8, 128)
-        x = bottleneck_asymmetric(x, 5, 128)
-        x = bottleneck_dilated(x, 16, 128)
+        x0 = SkipConvBlock(x0, 32, (3, 3), padding=(1, 1))
+    # 32 x 32 x 32
+    x1 = SkipConvBlockD(x0, 64, (3, 3), padding=(1, 1))
+    for _ in range(4):
+        x1 = SkipConvBlock(x1, 64, (3, 3), padding=(1, 1))
+    # 16 x 16 x 64
+    x2 = SkipConvBlockD(x1, 128, (3, 3), padding=(1, 1))
+    for _ in range(8):
+        x2 = SkipConvBlock(x2, 128, (3, 3), padding=(1, 1))
+    # 8 x 8 x 128
 
-    x = bottleneck_upsampling(x, 64)
-    x = Concatenate(axis=3)([x, x_skip_1])
-    x = bottleneck(x, 64)
-    x = bottleneck(x, 64)
-
-    # 32 x 32 x 64
-
-    x = bottleneck_upsampling(x, 16)
-    x = Concatenate(axis=3)([x, x_skip_0])
-    x = bottleneck(x, 16)
-
-    # 64 x 64 x 16
-
-    x = Conv2DTranspose(classes, kernel_size=(2, 2), strides=(2, 2), padding="same")(x)
-
-    # 128 x 128 x classes
+    x1 = UpSampling2D(size=(2, 2))(x1)
+    x2 = UpSampling2D(size=(4, 4))(x2)
+    x = Concatenate(axis=3)([x0, x1, x2])
+    # 32 x 32 x (32 + 64 + 128)
+    x = SkipConvBlockU(x, classes, (3, 3), padding=(1, 1), activation=False)
 
     outputs = Activation("softmax")(x)
     model = Model(inputs, outputs)
